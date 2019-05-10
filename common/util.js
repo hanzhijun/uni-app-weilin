4e381395aecd0c4a591926cdbbe3f0ba304676e6
Administrator <512804627@qq.com>
2019-05-10 16:22:03 +0800

    uni-app

--- common/util.js
+++ common/util.js
@@ -173,7 +173,6 @@ function audioPause(that, backgroundAudioManager) {
     } else if (warnState.warnNo == 3) {
         warnState.warnMotionTime = null
     }
-    warnState.warningText = ''
     warnState.warnNo = null
     changeWarn(that)
 }
@@ -190,6 +189,292 @@ function changeWarn(that) {
     that.warnBreathTime = warnState.warnBreathTime
     that.warnMotionTime = warnState.warnMotionTime
     that.warningText = warnState.warningText
+}
+
+/**
+ * 触发报警
+ * @param {Object} that 作用域
+ * @param {Object} backgroundAudioManager 背景音乐
+ */
+function audioStart(that, backgroundAudioManager) {
+    that.warnNing = 1
+    warnState.warnNing = 1
+    backgroundAudioManager.title = '报警'
+    backgroundAudioManager.epname = '报警'
+    backgroundAudioManager.singer = '报警'
+    backgroundAudioManager.coverImgUrl = ''
+    // 设置了 src 之后会自动播放
+    backgroundAudioManager.src = 'http://www.hanjiaxin.cn/images/warning.mp3';
+    backgroundAudioManager.onEnded(() => {
+        backgroundAudioManager.src = 'http://www.hanjiaxin.cn/images/warning.mp3?time=' + Date.parse(new Date());
+    });
+}
+
+/**
+ * 检验报警
+ * @param {Object} that 作用域
+ * @param {Object} res 数据
+ */
+function checkWarn(that, res, backgroundAudioManager) {
+
+    checkDevice(that, res, backgroundAudioManager)
+    checkHeart(that, res, backgroundAudioManager)
+    checkBreath(that, res, backgroundAudioManager)
+    checkMotion(that, res, backgroundAudioManager)
+
+}
+
+/**
+ * 离床检测
+ * @param {Object} that 作用域
+ * @param {Object} res 数据
+ */
+function checkDevice(that, res, backgroundAudioManager) {
+    // 如果正在报警，跳出检测
+    if (warnState.warnNing == 1) return
+    let warnRule = JSON.parse(getCookie('warnRule'))
+    let nowTime = Date.parse(new Date())
+    let nowTimeHour = new Date().getHours() + ':' + new Date().getMinutes()
+    let deviceStatus = res.successData[0].deviceStatus
+    let oldDeviceStatus = that.deviceStatus
+    // 系统设置为不监控 跳出检测
+    if (!warnRule.device) {
+        // console.log('系统设置为不监控 跳出检测')
+        warnState.warnDeviceTime = null
+        return
+    }
+    // 系统设置时间有误，跳出检测
+    if (warnRule.deviceStart > warnRule.deviceEnd) {
+        // console.log('系统设置时间有误，跳出检测')
+        warnState.warnDeviceTime = null
+        return
+    }
+    // 如果没有离床报警记录点时间戳，跳出检测
+    if (!warnState.warnDeviceTime) {
+        // console.log('如果没有离床报警记录点时间戳，跳出检测')
+        return
+    }
+    // 状态为4_在床，5_光纤故障，6_离线，9_传感器负荷，10_信号弱，则终止离床检测倒计时
+    if (deviceStatus == 4 || deviceStatus == 5 || deviceStatus == 6 || deviceStatus == 9 || deviceStatus == 10) {
+        // console.log('状态为4_在床，5_光纤故障，6_离线，9_传感器负荷，10_信号弱，则终止离床检测倒计时')
+        warnState.warnDeviceTime = null
+        return
+    }
+    // 不在报警时间段内，跳出检测
+    if (warnRule.deviceStart > nowTimeHour || warnRule.deviceEnd < nowTimeHour) {
+        // console.log('不在报警时间段内，跳出检测')
+        warnState.warnDeviceTime = null
+        return
+    }
+    console.log(nowTime - warnState.warnDeviceTime, '...离床监测..', warnRule.deviceTimes * 60 * 1000)
+    // 记录时间点已超出系统设置离床时间上限，触发离床报警
+    if ((nowTime - warnState.warnDeviceTime) > (warnRule.deviceTimes * 60 * 1000)) {
+        // console.log('记录时间点已超出系统设置离床时间上限，触发离床报警')
+        let t = new Date(warnState.warnDeviceTime)
+        that.warningText = '用户于' + (t.getHours() < 10 ? '0' + t.getHours() : t.getHours()) + ':' + (t.getMinutes() < 10 ?
+            '0' + t.getMinutes() : t.getMinutes()) + '离床，并已超过' + warnRule.deviceTimes + '分钟'
+        that.warning = 1
+        warnState.warnNing = 1
+        warnState.warnNo = 0
+        audioStart(that, backgroundAudioManager)
+        warnState.warnDeviceTime = null
+    }
+}
+
+/**
+ * 心率检测
+ * @param {Object} that 作用域
+ * @param {Object} res 数据
+ */
+function checkHeart(that, res, backgroundAudioManager) {
+    // 如果正在报警，跳出检测
+    if (warnState.warnNing == 1) return
+    let warnRule = JSON.parse(getCookie('warnRule'))
+    let nowTime = Date.parse(new Date())
+    let nowTimeHour = new Date().getHours() + ':' + new Date().getMinutes()
+    let deviceStatus = res.successData[0].deviceStatus
+    // 如果不是在床状态，跳出检测
+    if (deviceStatus != 4) {
+        // console.log('如果不是在床状态，跳出检测')
+        warnState.warnHeartTime = null
+        return
+    }
+    // 系统设置为不监控 跳出检测
+    if (!warnRule.heart) {
+        // console.log('系统设置为不监控 跳出检测')
+        warnState.warnHeartTime = null
+        return
+    }
+    // 系统设置上下限规则有误，跳出检测
+    if (warnRule.heartDown > warnRule.heartUp) {
+        // console.log('系统设置上下限规则有误，跳出检测')
+        warnState.warnHeartTime = null
+        return
+    }
+    // 如果没有心率报警记录点时间戳，设置记录点并跳出检测
+    if (!warnState.warnHeartTime) {
+        // console.log('如果没有心率报警记录点时间戳，跳出检测')
+        warnState.warnHeartTime = Date.parse(new Date())
+        return
+    }
+    // 心率回归正常值，初始化数据，并跳出检测
+    if (res.successData[0].heart <= warnRule.heartUp && res.successData[0].heart >= warnRule.heartDown) {
+        // console.log('心率回归正常值，初始化数据，并跳出检测')
+        warnState.warnHeartTime = null
+        return
+    }
+    // 记录时间点已超出系统默认设置心率持续异常时间上限，触发离床报警
+    if ((nowTime - warnState.warnHeartTime) > (30 * 1000)) {
+        if (res.successData[0].heart > warnRule.heartUp) {
+            // console.log('记录时间点已超出系统默认设置心率持续异常时间上限，触发心率报警')
+            let t = new Date(warnState.warnHeartTime)
+            that.warningText = '用户于' + nowTimeHour + '心率持续30秒高于您设置的上限峰值!'
+            that.warning = 1
+            warnState.warnNing = 1
+            warnState.warnNo = 1
+            audioStart(that, backgroundAudioManager)
+            warnState.warnHeartTime = null
+        } else {
+            // console.log('记录时间点已超出系统默认设置心率持续异常时间上限，触发心率报警')
+            let t = new Date(warnState.warnHeartTime)
+            that.warningText = '用户于' + nowTimeHour + '心率持续30秒低于您设置的下限峰值!'
+            that.warning = 1
+            warnState.warnNing = 1
+            warnState.warnNo = 1
+            audioStart(that, backgroundAudioManager)
+            warnState.warnHeartTime = null
+        }
+    }
+}
+
+/**
+ * 呼吸率检测
+ * @param {Object} that 作用域
+ * @param {Object} res 数据
+ */
+function checkBreath(that, res, backgroundAudioManager) {
+    // 如果正在报警，跳出检测
+    if (warnState.warnNing == 1) return
+    let warnRule = JSON.parse(getCookie('warnRule'))
+    let nowTime = Date.parse(new Date())
+    let nowTimeHour = new Date().getHours() + ':' + new Date().getMinutes()
+    let deviceStatus = res.successData[0].deviceStatus
+    // 如果不是在床状态，跳出检测
+    if (deviceStatus != 4) {
+        // console.log('如果不是在床状态，跳出检测')
+        warnState.warnBreathTime = null
+        return
+    }
+    // 系统设置为不监控 跳出检测
+    if (!warnRule.breath) {
+        // console.log('系统设置为不监控 跳出检测')
+        warnState.warnBreathTime = null
+        return
+    }
+    // 系统设置上下限规则有误，跳出检测
+    if (warnRule.breathDown > warnRule.breathUp) {
+        // console.log('系统设置上下限规则有误，跳出检测')
+        warnState.warnBreathTime = null
+        return
+    }
+    // 如果没有呼吸率报警记录点时间戳，设置记录点并跳出检测
+    if (!warnState.warnBreathTime) {
+        // console.log('如果没有呼吸率报警记录点时间戳，设置记录点并跳出检测')
+        warnState.warnBreathTime = Date.parse(new Date())
+        return
+    }
+    // 呼吸率回归正常值，初始化数据，并跳出检测
+    if (res.successData[0].breath <= warnRule.breathUp && res.successData[0].breath >= warnRule.breathDown) {
+        // console.log('呼吸率回归正常值，初始化数据，并跳出检测')
+        warnState.warnBreathTime = null
+        return
+    }
+    // 记录时间点已超出系统默认设置呼吸率持续异常时间上限，触发呼吸报警
+    if ((nowTime - warnState.warnBreathTime) > (30 * 1000)) {
+        if (res.successData[0].breath > warnRule.breathUp) {
+            // console.log('记录时间点已超出系统默认设置呼吸率持续异常时间上限，触发呼吸率报警')
+            let t = new Date(warnState.warnBreathTime)
+            that.warningText = '用户于' + (t.getHours() < 10 ? '0' + t.getHours() : t.getHours()) + ':' + (t.getMinutes() <
+                10 ? '0' + t.getMinutes() : t.getMinutes()) + '呼吸率持续30秒高于您设置的上限峰值!'
+            that.warning = 1
+            warnState.warnNing = 1
+            warnState.warnNo = 1
+            audioStart(that, backgroundAudioManager)
+            warnState.warnBreathTime = null
+        } else {
+            // console.log('记录时间点已超出系统默认设置呼吸率持续异常时间上限，触发呼吸率报警')
+            let t = new Date(warnState.warnBreathTime)
+            that.warningText = '用户于' + nowTimeHour + '呼吸率持续30秒低于您设置的下限峰值!'
+            that.warning = 1
+            warnState.warnNing = 1
+            warnState.warnNo = 1
+            audioStart(that, backgroundAudioManager)
+            warnState.warnBreathTime = null
+        }
+    }
+}
+
+/**
+ * 体动检测
+ * @param {Object} that 作用域
+ * @param {Object} res 数据
+ */
+function checkMotion(that, res, backgroundAudioManager) {
+    // console.log('时时体动数据 = ' + res.successData[0].motion)
+    // 如果正在报警，跳出检测
+    if (warnState.warnNing == 1) return
+    let warnRule = JSON.parse(getCookie('warnRule'))
+    let nowTime = Date.parse(new Date())
+    let nowTimeHour = new Date().getHours() + ':' + new Date().getMinutes()
+    let deviceStatus = res.successData[0].deviceStatus
+    // 如果不是在床状态，跳出检测
+    if (deviceStatus != 4) {
+        // console.log('如果不是在床状态，跳出检测')
+        warnState.warnBreathTime = null
+        return
+    }
+    // 系统设置为不监控 跳出检测
+    if (!warnRule.motion) {
+        // console.log('系统设置为不监控 跳出检测')
+        warnState.warnMotionTime = null
+        return
+    }
+    // 系统设置时间有误，跳出检测
+    if (warnRule.motionStart > warnRule.motionEnd) {
+        // console.log('系统设置时间有误，跳出检测')
+        warnState.warnMotionTime = null
+        return
+    }
+    // 如果没有体动报警记录点时间戳，跳出检测
+    if (!warnState.warnMotionTime) {
+        warnState.warnMotionTime = Date.parse(new Date())
+        return
+    }
+    // 不在报警时间段内，跳出检测
+    if (warnRule.motionStart > nowTimeHour || warnRule.motionEnd < nowTimeHour) {
+        // console.log('不在报警时间段内，跳出检测')
+        warnState.warnMotionTime = null
+        return
+    }
+    // 体动值回复正常数据，初始化数据并跳出检测
+    if (res.successData[0].motion < 2) {
+        // console.log('体动值回复正常数据，初始化数据并跳出检测')
+        warnState.warnMotionTime = null
+        return
+    }
+    console.log(nowTime - warnState.warnMotionTime, '...体动监测..', warnRule.motionTimes * 60 * 1000)
+    // 记录时间点已超出系统设置体动时间上限，触发体动报警
+    if ((nowTime - warnState.warnMotionTime) > (warnRule.motionTimes * 60 * 1000)) {
+        // console.log('记录时间点已超出系统设置体动时间上限，触发体动报警')
+        let t = new Date(warnState.warnMotionTime)
+        that.warningText = '用户于' + (t.getHours() < 10 ? '0' + t.getHours() : t.getHours()) + ':' + (t.getMinutes() < 10 ?
+            '0' + t.getMinutes() : t.getMinutes()) + '开始体动频繁，并已超过' + warnRule.motionTimes + '分钟！'
+        that.warning = 1
+        warnState.warnNing = 1
+        warnState.warnNo = 0
+        audioStart(that, backgroundAudioManager)
+        warnState.warnMotionTime = null
+    }
 }
 
 /**
@@ -219,5 +504,6 @@ module.exports = {
     setWarnCookie,
     audioPause,
     changeWarn,
-    showToastBox
+    showToastBox,
+    checkWarn
 }
